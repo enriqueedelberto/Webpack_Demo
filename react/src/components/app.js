6ab67c27f6e5fc3a2d149507b1df9213fdf8c985
enriqueedelberto <enriqueviiireyes@gmail.com>
2019-07-21 16:48:12 -0500

    Add some react stuff

--- react/src/components/app.js
+++ react/src/components/app.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './loader';
 
+
+console.log(data)
 
 function App() {
-    return ( < div > Hello World: This is an awesome example with React < /div>)
+    const [loaderList, setLoaderList] = useState([]) // this is a Hook
+    function handleClick() {
+        setLoaderList(data.loaders)
+    }
+    return ( < div >
+        Hello World: This is an awesome example with React <
+        ul > {
+            loaderList.map((item) => < Loader {...item }
+                key = { item.id }
+                />)
+            } <
+            /ul>   <
+            button onClick = { handleClick } > Show what you have learn so far < /button>  < /
+            div > )
     }
 
     export default App
