a3288f48e93f287745fecfc0898ab750401226d7
enriqueedelberto <enriqueviiireyes@gmail.com>
2019-07-20 21:14:27 -0500

    Add more than one configuration in package.json

--- external/webpack.config.js
+++ external/webpack.config.js
@@ -1,4 +1,4 @@
-const path = required('path');
+const path = require('path');
 
 module.exports = {
     entry: path.resolve(__dirname, './index.js'),
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-const path = required('path');
+const path = require('path');
 
 module.exports = {
     entry: './index.js',
-    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
